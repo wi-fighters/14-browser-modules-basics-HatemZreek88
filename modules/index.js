683cdeaf50dff0d0942b5529adbeb22a87789c09
HatemZreek88 <hatem.zreek88@gmail.com>
2020-02-12 13:32:25 +0100

    modules

---
+++ modules/index.js
@@ -0,0 +1,8 @@
+// step 2
+import { clickImages } from "./click.js";
+import { hoverHeader } from "./hover.js";
+import { scrollWindow } from "./scroll.js";
+// step 3
+clickImages();
+hoverHeader();
+scrollWindow();
